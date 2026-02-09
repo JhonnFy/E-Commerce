85f0b908bc10e0d290d7150dabf76ebf773552fb
JhonFy <moesdeveloper1023@gmail.com>
2026-02-09 14:32:17 -0500

    Vista Category Terminada

---
+++ ECommerce/ECommerce/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ECommerce.Context;
+using ECommerce.Entities;
+
+namespace ECommerce.Controllers
+{
+    public class CategoryController(AppDbContext _dbContext) : Controller
+    {
+        public IActionResult Index()
+        {
+            /*Consultar Todas Las Categorias. Category se usa en el llamado de la vista*/
+            var categories = _dbContext.Category.ToList();
+            /*Retornar La Vista Objeto*/
+            return View(categories);
+        }
+    }
+}
+
+
