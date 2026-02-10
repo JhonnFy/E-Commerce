fcabb09fbcfede1b89da8899ed882fc71a466339
JhonFy <moesdeveloper1023@gmail.com>
2026-02-10 05:05:48 -0500

    Crear El Metodo GetAllAsync En ProductServices

--- ECommerce/ECommerce/Services/ProductServices.cs
+++ ECommerce/ECommerce/Services/ProductServices.cs
@@ -1,6 +1,7 @@
 ﻿using ECommerce.Entities;
 using ECommerce.Models;
 using ECommerce.Repositories;
+using System.Linq.Expressions;
 
 namespace ECommerce.Services
 {
@@ -18,10 +19,28 @@ namespace ECommerce.Services
         /*Metodo Para Listar Productos*/
         public async Task<IEnumerable<ProductVM>>GetAllAsync()
         {
-            var products = await _productRepository.GetAllAsync();
+            var products = await _productRepository.GetAllAsync(
+                includes: new Expression<Func<Product, object>>[] {x => x.Category!}
+                );
+
+            var productVM = products.Select(item =>
+                new ProductVM
+                {
+                    ProductId = item.ProductId,
+                    Category = new CategoryVM
+                    {
+                        CategoryId = item.Category!.CategoryId,
+                        Name = item.Category!.Name,
+                    },
+                    Name = item.Name,
+                    Description = item.Description,
+                    Price = item.Price,
+                    Stock = item.Stock,
+                    ImageName = item.ImageName,
+                }).ToList();
+
+            return productVM;
         }
-
-
     }
             
 
