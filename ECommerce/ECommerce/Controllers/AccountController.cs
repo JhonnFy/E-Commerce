4a6451c1ee296a5c84bb2013a685ce3d980a8fe0
JhonFy <moesdeveloper1023@gmail.com>
2026-02-12 21:01:20 -0500

    Login V1

---
+++ ECommerce/ECommerce/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ECommerce.Models;
+using ECommerce.Services;
+
+namespace ECommerce.Controllers
+{
+    public class AccountController(UserService _userService) : Controller
+    {
+        public IActionResult Login()
+        {
+            var viewModel = new LoginVM();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>Login(LoginVM viewmodel)
+        {
+            if(!ModelState.IsValid) return View(viewmodel);
+            var found = await _userService.Login(viewmodel);
+
+            if(found.UserId ==0)
+            {
+                ViewBag.message = "No Matches Found";
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+
+        }
+    }
+}
